2093f32593503febfdd78cf41426d2dcbb329348
William Quiñones <william.quinones@believesol.com>
2025-04-01 17:14:16 -0400

    Feat: Configurando Swagger y levantando en localhost:8787/api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Login con NestJS')
+    .setDescription('Login API description')
+    .setVersion('1.0')
+    .addTag('Login')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        name: 'Authorization',
+        description: 'Agrega tu token de autenticación JWT',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
